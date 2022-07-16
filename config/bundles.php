e0ecc39f589331b70d1142f9896a6738fdee35a5
Thomas Durand <thomas.durand.verjat@gmail.com>
2022-07-16 08:08:31 +0200

    Added debug & maker

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
