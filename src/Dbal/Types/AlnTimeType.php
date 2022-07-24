be6da098d44fa494ed3d4fa92acc64232695e1b7
Thomas Durand <thomas.durand.verjat@gmail.com>
2022-07-24 12:07:35 +0200

    Renamed Doctrine namespace to Dbal

--- src/Dbal/Types/AlnTimeType.php
+++ src/Dbal/Types/AlnTimeType.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Doctrine\Types;
+namespace App\Dbal\Types;
 
 use Doctrine\DBAL\Platforms\AbstractPlatform;
 use Doctrine\DBAL\Types\ConversionException;
--- src/Entity/AlnMeal.php
+++ src/Entity/AlnMeal.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
-use App\Doctrine\Types\AlnTimeType;
+use App\Dbal\Types\AlnTimeType;
 use App\Repository\AlnMealRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
