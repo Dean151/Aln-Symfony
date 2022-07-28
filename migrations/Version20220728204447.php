98a16aa392aabe032f8ad5befd1c5b338ee9d74c
Thomas Durand <thomas.durand.verjat@gmail.com>
2022-07-28 22:45:21 +0200

    Simplified migration

--- migrations/Version20220728204447.php
+++ migrations/Version20220728204447.php
@@ -10,23 +10,32 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220726192833 extends AbstractMigration
+final class Version20220728204447 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Added authentication tables';
     }
 
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE reset_password_request (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, selector VARCHAR(20) NOT NULL, hashed_token VARCHAR(100) NOT NULL, requested_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', expires_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_7CE748AA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, identifier VARCHAR(64) NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649772E836A (identifier), UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE reset_password_request ADD CONSTRAINT FK_7CE748AA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE aln_feeder ADD owner_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE aln_feeder ADD CONSTRAINT FK_1DDCA11A7E3C61F9 FOREIGN KEY (owner_id) REFERENCES user (id)');
+        $this->addSql('CREATE INDEX IDX_1DDCA11A7E3C61F9 ON aln_feeder (owner_id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE aln_feeder DROP FOREIGN KEY FK_1DDCA11A7E3C61F9');
+        $this->addSql('ALTER TABLE reset_password_request DROP FOREIGN KEY FK_7CE748AA76ED395');
         $this->addSql('DROP TABLE reset_password_request');
+        $this->addSql('DROP TABLE user');
+        $this->addSql('DROP INDEX IDX_1DDCA11A7E3C61F9 ON aln_feeder');
+        $this->addSql('ALTER TABLE aln_feeder DROP owner_id');
     }
 }
