771d388ee3f1c70ac8da22ff038c679cacd16206
Thomas Durand <thomas.durand.verjat@gmail.com>
2022-08-26 13:46:03 +0200

    Booted email unsubscription mecanism

---
+++ src/Email/AbstractEmailFactory.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Email;
+
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\DependencyInjection\ParameterBag\ContainerBagInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+abstract class AbstractEmailFactory
+{
+    private TranslatorInterface $translator;
+    private string $senderEmail;
+    private string $unsubscribeEmail;
+    private string $unsubscribeUrl;
+
+    protected string $siteName;
+    protected string $siteBaseUrl;
+
+    public function __construct(ContainerBagInterface $params, TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+        $this->senderEmail = $params->get('email.sender');
+        $this->unsubscribeEmail = $params->get('email.unsubscribe');
+        $this->unsubscribeUrl = $params->get('api.base_url');
+        $this->siteBaseUrl = $params->get('site.base_url');
+        $this->siteName = $params->get('site.name');
+    }
+
+    /**
+     * @param array<string, string> $context
+     */
+    protected function createTemplatedEmail(string $recipientEmail, string $subject, string $template, array $context): TemplatedEmail
+    {
+        $email = new TemplatedEmail();
+        $this->setUnsubscribeHeaders($email, $recipientEmail);
+
+        return $email
+            ->to($recipientEmail)
+            ->from($this->senderEmail)
+            ->subject($subject)
+            ->htmlTemplate($template)
+            ->context($context);
+    }
+
+    private function setUnsubscribeHeaders(Email $email, string $recipientEmail): void
+    {
+        // FIXME: generate a token that we can check back afterward
+        $unsubscribeToken = strtr(base64_encode(random_bytes(16)), ['+' => '-', '/' => '_', '=' => '']);
+        $unsubscribeMailto = "mailto:{$this->unsubscribeEmail}?subject=unsubscribe&body={$unsubscribeToken}";
+        $unsubscribeUrl = "{$this->unsubscribeUrl}?email={$recipientEmail}&token={$unsubscribeToken}";
+        $unsubscribeBody = sprintf('<%s>, <%s>', $unsubscribeMailto, $unsubscribeUrl);
+        $email->getHeaders()->addTextHeader('List-Unsubscribe', $unsubscribeBody);
+        $email->getHeaders()->addTextHeader('List-Unsubscribe-Post', 'List-Unsubscribe=One-Click');
+    }
+
+    protected function getLocale(): string
+    {
+        return $this->translator->getLocale();
+    }
+
+    /**
+     * @param array<string, string> $context
+     */
+    protected function translate(string $id, array $context, string $domain = null, string $locale = null): string
+    {
+        return $this->translator->trans($id, $context, $domain, $locale);
+    }
+}
--- src/Email/NewPasswordEmailFactory.php
+++ src/Email/NewPasswordEmailFactory.php
@@ -4,51 +4,29 @@ declare(strict_types=1);
 
 namespace App\Email;
 
-use Symfony\Bridge\Twig\Mime\TemplatedEmail;
-use Symfony\Component\DependencyInjection\ParameterBag\ContainerBagInterface;
 use Symfony\Component\Mime\Email;
-use Symfony\Contracts\Translation\TranslatorInterface;
 use SymfonyCasts\Bundle\ResetPassword\Model\ResetPasswordToken;
 
-final class NewPasswordEmailFactory
+final class NewPasswordEmailFactory extends AbstractEmailFactory
 {
-    private TranslatorInterface $translator;
-    private string $senderEmail;
-    private string $siteName;
-    private string $siteBaseUrl;
-
-    public function __construct(ContainerBagInterface $params, TranslatorInterface $translator)
-    {
-        $this->translator = $translator;
-        $this->senderEmail = $params->get('email.sender');
-        $this->siteBaseUrl = $params->get('site.base_url');
-        $this->siteName = $params->get('site.name');
-    }
-
-    public function create(string $type, string $recipientAddress, ResetPasswordToken $token): Email
+    public function create(string $type, string $recipientEmail, ResetPasswordToken $token): Email
     {
         if (!in_array($type, ['register', 'reset_password'])) {
             throw new \InvalidArgumentException('$type is invalid');
         }
 
-        $locale = $this->translator->getLocale();
-
-        $email = new TemplatedEmail();
-        $email->from($this->senderEmail);
-        $email->to($recipientAddress);
-
-        $email->subject($this->translator->trans(sprintf('%s.subject', $type), ['%site_name%' => $this->siteName]));
-        $email->htmlTemplate(sprintf('emails/%s_%s.html.twig', $type, $locale));
-        $email->context([
-            'url' => $this->buildUrl($token, $locale),
-            'recipient_email' => $recipientAddress,
+        $subject = $this->translate(sprintf('%s.subject', $type), ['%site_name%' => $this->siteName]);
+        $template = sprintf('emails/%s_%s.html.twig', $type, $this->getLocale());
+        $context = [
+            'url' => $this->buildUrl($token),
+            'recipient_email' => $recipientEmail,
             'site_name' => $this->siteName,
-        ]);
+        ];
 
-        return $email;
+        return $this->createTemplatedEmail($recipientEmail, $subject, $template, $context);
     }
 
-    private function buildUrl(ResetPasswordToken $token, string $locale): string
+    private function buildUrl(ResetPasswordToken $token): string
     {
         return "{$this->siteBaseUrl}/reset/{$token->getToken()}";
     }
