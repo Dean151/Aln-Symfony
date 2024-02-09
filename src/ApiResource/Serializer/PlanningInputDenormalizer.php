0c06a4405c3f958943b0e069ab8b00795609189c
Thomas Durand <thomas.durand.verjat@gmail.com>
2024-02-10 00:58:19 +0100

    Fixed denormalized for Symfony 7

--- src/ApiResource/Serializer/PlanningInputDenormalizer.php
+++ src/ApiResource/Serializer/PlanningInputDenormalizer.php
@@ -6,14 +6,15 @@ namespace App\ApiResource\Serializer;
 
 use App\ApiResource\Dto\PlanningInput;
 use App\Entity\AlnFeeder;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 
 final class PlanningInputDenormalizer implements DenormalizerInterface
 {
     public function __construct(
-        private readonly ObjectNormalizer $normalizer,
+        #[Autowire(service: 'serializer.normalizer.object')]
+        private readonly DenormalizerInterface $denormalizer,
     ) {
     }
 
@@ -42,7 +43,7 @@ final class PlanningInputDenormalizer implements DenormalizerInterface
      */
     public function denormalize(mixed $data, string $type, ?string $format = null, array $context = []): PlanningInput
     {
-        $planning = $this->normalizer->denormalize($data, PlanningInput::class, $format, $context);
+        $planning = $this->denormalizer->denormalize($data, PlanningInput::class, $format, $context);
         assert($context[AbstractNormalizer::OBJECT_TO_POPULATE] instanceof AlnFeeder);
         $planning->feeder = $context[AbstractNormalizer::OBJECT_TO_POPULATE];
 
