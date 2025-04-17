0dcfdc8c4dd21c2c698c4d876d4e9f7ab29426e6
Thomas Durand <thomas.durand.verjat@gmail.com>
2025-04-18 00:33:40 +0200

    Fix one test

--- src/ApiResource/Provider/AlnFeederProvider.php
+++ src/ApiResource/Provider/AlnFeederProvider.php
@@ -8,6 +8,7 @@ use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProviderInterface;
 use App\Entity\AlnFeeder;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
  * Temporary provider to fix an issue starting from ApiPlatform 3.1.11
@@ -26,11 +27,11 @@ final class AlnFeederProvider implements ProviderInterface
     ) {
     }
 
-    public function provide(Operation $operation, array $uriVariables = [], array $context = []): ?AlnFeeder
+    public function provide(Operation $operation, array $uriVariables = [], array $context = []): AlnFeeder
     {
         $feeder = $this->itemProvider->provide($operation, $uriVariables, $context);
         if (is_null($feeder)) {
-            return null;
+            throw new NotFoundHttpException();
         }
 
         assert($feeder instanceof AlnFeeder);
