b0a853437cafab8a8987b8c1ff6b53b8291c2883
Thomas Durand <thomas.durand.verjat@gmail.com>
2025-04-21 13:22:28 +0200

    chore: updated api platform

--- src/ApiResource/Dto/EmailInput.php
+++ src/ApiResource/Dto/EmailInput.php
@@ -6,50 +6,48 @@ namespace App\ApiResource\Dto;
 
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Post;
+use ApiPlatform\OpenApi\Model\Operation;
+use ApiPlatform\OpenApi\Model\Response;
 use App\Controller\RegisterUser;
 use App\Controller\ResetPassword;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Response as HttpResponse;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ApiResource(
     operations: [
         new Post(
             uriTemplate: '/user/register',
-            status: Response::HTTP_OK,
+            status: HttpResponse::HTTP_OK,
             controller: RegisterUser::class,
-            openapiContext: [
-                'tags' => ['User'],
-                'summary' => 'Register using an email',
-                'description' => 'Register using an email',
-                'responses' => [
-                    'responses' => [
-                        Response::HTTP_OK => [
-                            'description' => 'Register email has been sent.',
-                        ],
-                        Response::HTTP_CONFLICT => [
-                            'description' => 'Email address already in use.',
-                        ],
-                    ],
+            openapi: new Operation(
+                tags: ['User'],
+                responses: [
+                    HttpResponse::HTTP_OK => new Response(
+                        description: 'Register email has been sent.',
+                    ),
+                    HttpResponse::HTTP_CONFLICT => new Response(
+                        description: 'Email address already in use.',
+                    ),
                 ],
-            ],
+                summary: 'Register using an email',
+            ),
         ),
         new Post(
             uriTemplate: '/user/reset',
-            status: Response::HTTP_OK,
+            status: HttpResponse::HTTP_OK,
             controller: ResetPassword::class,
-            openapiContext: [
-                'tags' => ['User'],
-                'summary' => 'Request password reset for an account',
-                'description' => 'Request password reset for an account',
-                'responses' => [
-                    Response::HTTP_OK => [
-                        'description' => 'Reset password instruction email has been sent.',
-                    ],
-                    Response::HTTP_NOT_FOUND => [
-                        'description' => 'Email not found.',
-                    ],
+            openapi: new Operation(
+                tags: ['User'],
+                responses: [
+                    HttpResponse::HTTP_OK => new Response(
+                        description: 'Reset password instruction email has been sent.',
+                    ),
+                    HttpResponse::HTTP_NOT_FOUND => new Response(
+                        description: 'Email address not found.',
+                    ),
                 ],
-            ],
+                summary: 'Request password reset for an account',
+            ),
         ),
     ],
 )]
